a2f7d9a2f4bf080c14e2f580a83546856c98092a
Jason Luo <jasonl960523@gmail.com>
2017-04-22 18:39:28 +0800

    Configure controllers to respond to json request

--- App/RailsApp/app/controllers/application_controller.rb
+++ App/RailsApp/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, prepend: true
 
+  respond_to :json
+
   before_action :underscore_params!
 
   private
