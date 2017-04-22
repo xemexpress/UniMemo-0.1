7fc89afa824a35f1dd7ecef4a66ced1801f2bb11
Jason Luo <jasonl960523@gmail.com>
2017-04-22 18:37:55 +0800

    Create a filter to convert incoming params to snake_case

--- App/RailsApp/app/controllers/application_controller.rb
+++ App/RailsApp/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, prepend: true
+
+  before_action :underscore_params!
+
+  private
+
+  def underscore_params!
+    params.transform_keys!(&:underscore)
+  end
 end
