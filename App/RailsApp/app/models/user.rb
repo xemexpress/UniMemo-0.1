100a2514314006b0e2cd502f54d7b41dfa255b27
Jason Luo <jasonl960523@gmail.com>
2017-04-22 19:13:14 +0800

    Create generate_jwt method for User
    To generate JWT tokens. After decoding the tokens, User's id is retrieved.

--- App/RailsApp/app/models/user.rb
+++ App/RailsApp/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def generate_jwt
+    JWT.encode({ id: id,
+                exp: 60.days.from_now.to_i },
+                Rails.application.secrets.secret_key_base)
+  end
+  
 end
