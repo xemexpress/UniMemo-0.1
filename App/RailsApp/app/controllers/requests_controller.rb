cdc1d13ef2ad5c6a2557bbfd8d569d74b59d8bb5
Jason Luo <jasonl960523@gmail.com>
2017-04-23 23:18:35 +0800

    Reserve for users who signed in
    It needs more discussions on this.

--- App/RailsApp/app/controllers/requests_controller.rb
+++ App/RailsApp/app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!#, except: [:index, :show]
 
   def index
     @requests = Request.all.includes(:poster)
