826ea96f9bf59b5cb18bfc6b0c3fb43034c98b43
Jason Luo <jasonl960523@gmail.com>
2017-04-23 09:19:19 +0800

    Update the Devise routes
    Use sessions_controller.rb
    Sign in to the Path login

--- App/RailsApp/config/routes.rb
+++ App/RailsApp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
-    devise_for :users
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
