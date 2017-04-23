bb62a57452d1f7ede6114664bbea8ac40e1fd8f2
Jason Luo <jasonl960523@gmail.com>
2017-04-24 02:14:30 +0800

    Permit users to update `mobileNum`
    null to 0 in User's `mem`, `greyStars`, `yellowStars`

--- App/RailsApp/app/controllers/users_controller.rb
+++ App/RailsApp/app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :bio, :proPic)
+    params.require(:user).permit(:username, :email, :password, :bio, :proPic, :mobileNum)
   end
 end
