f0bd0c997376bd81eaa16b3bd071e9c5d01a0491
Jason Luo <jasonl960523@gmail.com>
2017-04-23 09:34:11 +0800

    Create authenticate_user method for User's JWTs

--- App/RailsApp/app/controllers/application_controller.rb
+++ App/RailsApp/app/controllers/application_controller.rb
@@ -5,11 +5,26 @@ class ApplicationController < ActionController::Base
 
   before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def authenticate_user
+    if request.headers['Authorization'].present?
+      authenticate_or_request_with_http_token do |token|
+        begin
+          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+
+          @current_user_id = jwt_payload['id']
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+          head :unauthorized
+        end
+      end
+    end
   end
 
   def underscore_params!
