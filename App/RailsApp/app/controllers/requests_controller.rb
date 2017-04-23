c0d0a351edbd21324e20ab83470ae6346e557d3a
Jason Luo <jasonl960523@gmail.com>
2017-04-23 21:28:42 +0800

    Create the index action for RequestsController
    Retrieving a list of requests.
    posted_by() and helped_by() are temporarily reserved due to its aggressiveness.

--- App/RailsApp/app/controllers/requests_controller.rb
+++ App/RailsApp/app/controllers/requests_controller.rb
@@ -1,6 +1,20 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @requests = Request.all.includes(:poster)
+
+    # This corrupts Privacy, and may hurt Users due to others' limited understanding of the whole picture.
+    #@requests = @requests.posted_by(params[:poster]) if params[:poster].present?
+
+    # This is considered less aggressive. But still, it can indirectly harm Users.
+    #@requests = @requests.helped_by(params[:helper]) if params[:helper].present?
+
+    @requests_count = @requests.count
+
+    @requests = @requests.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 10)
+  end
+
   private
 
   def request_params
