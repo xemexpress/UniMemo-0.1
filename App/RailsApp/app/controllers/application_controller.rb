af9a518dbdfc8f83a3c907ec8454daf4dd77fc61
Jason Luo <jasonl960523@gmail.com>
2017-04-23 09:23:37 +0800

    Configure Devise to allow usernames on signup

--- App/RailsApp/app/controllers/application_controller.rb
+++ App/RailsApp/app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::Base
   respond_to :json
 
   before_action :underscore_params!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   def underscore_params!
     params.transform_keys!(&:underscore)
