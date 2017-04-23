d0c1dd972e556a5ef8a6816c298a6bf855cec827
Jason Luo <jasonl960523@gmail.com>
2017-04-23 18:52:01 +0800

    Add a before_validation callback to generate request_id

--- App/RailsApp/app/models/request.rb
+++ App/RailsApp/app/models/request.rb
@@ -4,4 +4,8 @@ class Request < ApplicationRecord
 
   validates :text, presence: true, allow_blank: false
   validates :request_id, uniqueness: true
+
+  before_validation do
+    self.request_id ||= "#{rand(36**3).to_s(36)}#{id.to_s(36)}#{rand(36**3).to_s(36)}"
+  end
 end
