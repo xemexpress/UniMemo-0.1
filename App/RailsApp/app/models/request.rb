7384798b08f53773ea7d2b0825cb4c05a10003c2
Jason Luo <jasonl960523@gmail.com>
2017-04-24 02:04:29 +0800

    Add :class_name to belongs_to associations
    Fix errors

--- App/RailsApp/app/models/request.rb
+++ App/RailsApp/app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
-  belongs_to :poster
-  belongs_to :helper
+  belongs_to :poster, :class_name => "User"
+  belongs_to :helper, :class_name => "User"
 
   scope :posted_by, ->(username) { where(poster: User.where(username: username)) }
   scope :helped_by, ->(username) { where(helper: User.where(username: username)) }
