5d06c8449feb39d66fd31168babf9300cbf68ce4
Jason Luo <jasonl960523@gmail.com>
2017-04-23 16:39:12 +0800

    Define resources for profiles
    params[:id] is replaced by params[:username]

--- App/RailsApp/config/routes.rb
+++ App/RailsApp/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { sessions: :sessions },
                        path_names: { sign_in: :login }
     resource :user, only: [:show, :update]
+
+    resources :profiles, param: :username, only: [:show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
