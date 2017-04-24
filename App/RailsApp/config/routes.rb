66f694bb9cd3c559914dcdf4a82a7f6a0b4835e8
Jason Luo <jasonl960523@gmail.com>
2017-04-24 22:40:08 +0800

    Nest Favor's resource in Profiles's resources

--- App/RailsApp/config/routes.rb
+++ App/RailsApp/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
                        path_names: { sign_in: :login }
     resource :user, only: [:show, :update]
 
-    resources :profiles, param: :username, only: [:show]
+    resources :profiles, param: :username, only: [:show] do
+      resource :favor, only: [:create, :destroy]
+    end
 
     resources :requests, param: :request_id, except: [:edit, :new] do
       resource :wish, only: [:create, :destroy]
