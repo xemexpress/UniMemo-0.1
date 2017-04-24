021bb862817ecdefef7e9bafc19c7946c12f9e04
Jason Luo <jasonl960523@gmail.com>
2017-04-24 16:07:25 +0800

    Add a has_many association for wishes to Request

--- App/RailsApp/app/models/request.rb
+++ App/RailsApp/app/models/request.rb
@@ -1,6 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :poster, :class_name => "User"
   belongs_to :helper, :class_name => "User"
+  has_many :wishes, dependent: :destroy
 
   scope :posted_by, ->(username) { where(poster: User.where(username: username)) }
   scope :helped_by, ->(username) { where(helper: User.where(username: username)) }
