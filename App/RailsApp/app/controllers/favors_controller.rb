a93aa59a7cb744bf5e785900a0bbcd583211f985
Jason Luo <jasonl960523@gmail.com>
2017-04-24 23:31:57 +0800

    Create the destroy action for FavorsController

--- App/RailsApp/app/controllers/favors_controller.rb
+++ App/RailsApp/app/controllers/favors_controller.rb
@@ -4,7 +4,15 @@ class FavorsController < ApplicationController
   def create
     @user = User.find_by_username!(params[:profile_username])
 
-    current_user.follow(@user) if current_user.id != @user.id
+    current_user.follow(@user) if @current_user_id != @user.id
+
+    render 'profiles/show'
+  end
+
+  def destroy
+    @user = User.find_by_username!(params[:profile_username])
+
+    current_user.stop_following(@user) if @current_user_id != @user.id
 
     render 'profiles/show'
   end
