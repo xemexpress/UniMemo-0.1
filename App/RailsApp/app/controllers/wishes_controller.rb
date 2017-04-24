c18061c59cef3dbfa1ab9b3a3358950ab9dc71a9
Jason Luo <jasonl960523@gmail.com>
2017-04-24 16:56:31 +0800

    Create the create and destroy actions for WishesController

--- App/RailsApp/app/controllers/wishes_controller.rb
+++ App/RailsApp/app/controllers/wishes_controller.rb
@@ -2,6 +2,18 @@ class WishesController < ApplicationController
   before_action :authenticate_user!
   before_action :find_request!
 
+  def create
+    current_user.wish(@request)
+
+    render 'requests/show'
+  end
+
+  def destroy
+    current_user.unwish(@request)
+
+    render 'requests/show'
+  end
+
   private
 
   def find_request!
