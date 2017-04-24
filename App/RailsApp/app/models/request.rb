ee6a6bca4b1aa678ed5fc1afa0d2d581620084c9
Jason Luo <jasonl960523@gmail.com>
2017-04-24 16:26:02 +0800

    Add a scope for querying a User's wished requests by their usernames

--- App/RailsApp/app/models/request.rb
+++ App/RailsApp/app/models/request.rb
@@ -5,6 +5,7 @@ class Request < ApplicationRecord
 
   scope :posted_by, ->(username) { where(poster: User.where(username: username)) }
   scope :helped_by, ->(username) { where(helper: User.where(username: username)) }
+  scope :wished_by, ->(username) { joins(:wishes).where(wishes: { user: User.where(username: username) }) }
 
   acts_as_taggable
 
