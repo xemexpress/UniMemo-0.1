1554b083f2b3190c3de9795c62f3da3eb9172c12
Jason Luo <jasonl960523@gmail.com>
2017-04-24 12:07:26 +0800

    Add resources only with the index action for tags

--- App/RailsApp/config/routes.rb
+++ App/RailsApp/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :profiles, param: :username, only: [:show]
 
     resources :requests, param: :request_id, except: [:edit, :new]
+
+    resources :tags, only: [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
