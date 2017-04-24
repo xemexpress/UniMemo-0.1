4ef7ddfe5f15662fe62a4735fb028bfc9d2f4184
Jason Luo <jasonl960523@gmail.com>
2017-04-24 23:46:58 +0800

    Nest the collect's route in Requests's resources
    Resolve to GET /api/requests/collect

--- App/RailsApp/config/routes.rb
+++ App/RailsApp/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :requests, param: :request_id, except: [:edit, :new] do
       resource :wish, only: [:create, :destroy]
       resources :comments, except: [:edit, :new, :show]
+      get :collect, on: :collection
     end
 
     resources :tags, only: [:index]
