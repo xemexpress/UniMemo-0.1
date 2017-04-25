2645f52b6aa0e0b6021c0c7c0a47ee2c20cf2d9c
Jason Luo <jasonl960523@gmail.com>
2017-04-25 10:25:20 +0800

    Create the create action for TakesController
    Allow Users to follow/take Request

--- App/RailsApp/app/controllers/takes_controller.rb
+++ App/RailsApp/app/controllers/takes_controller.rb
@@ -1,4 +1,11 @@
 class TakesController < ApplicationController
   before_action :authenticate_user!
-  
+
+  def create
+    @request = Request.find_by_request_id!(params[:request_request_id])
+
+    current_user.follow(@request) if @current_user_id != @request.poster_id
+
+    render 'requests/show'
+  end
 end
