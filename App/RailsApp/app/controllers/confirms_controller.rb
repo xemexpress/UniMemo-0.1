1a82586485f40ba739b11d0aab397e832e8e870d
Jason Luo <jasonl960523@gmail.com>
2017-04-25 11:31:19 +0800

    Create ConfirmsController
    Authentication and the Request are checked before other actions

---
+++ App/RailsApp/app/controllers/confirms_controller.rb
@@ -0,0 +1,10 @@
+class ConfirmsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_request!
+
+  private
+
+  def find_request!
+    @request = Request.find_by_request_id!(params[:request_request_id])
+  end
+end
