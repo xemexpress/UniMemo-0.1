e34d2c29e198dc8c7ce2e90087f37e3d42bba710
Jason Luo <jasonl960523@gmail.com>
2017-05-19 10:19:55 +0800

    Init

--- UniMemo App/react-app/src/index.js
+++ UniMemo App/react-app/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-class App extends React.Component {
-  render(){
-    return (
-      <h1>Hello, World!</h1>
-    )
+import App from './components/App'
+
+import {
+
+} from './constants/actionTypes'
+
+const defaultState = {
+  appName: 'UniMemo',
+  requests: null
+}
+const reducer = (state=defaultState, action) => {
+  switch(action.type){
+    default:
   }
+  return state
 }
 
+const store = createStore(reducer)
+
 ReactDOM.render((
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.getElementById('root'))
