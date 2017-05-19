dc0552d54dbf590d93fd071e8fb3314381884f6c
Jason Luo <jasonl960523@gmail.com>
2017-05-19 09:53:47 +0800

    Clean-up

--- UniMemo App/react-app/src/index.js
+++ UniMemo App/react-app/src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  render(){
+    return (
+      <h1>Hello, World!</h1>
+    )
+  }
+}
+
+ReactDOM.render((
+  <App />
+), document.getElementById('root'))
