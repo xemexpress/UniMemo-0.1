13a665c13ba525c0079f65d486d2d001612fc1aa
Jason Luo <jasonl960523@gmail.com>
2017-05-19 12:17:22 +0800

    Create RequestList

---
+++ UniMemo App/react-app/src/components/RequestList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const RequestList = props => {
+  if(!props.requests){
+    return (
+      <div className='article-preview'>
+        Loading...
+      </div>
+    )
+  }
+
+  if(props.requests.length === 0){
+    return (
+      <div className='article-preview'>
+        No requests are here... yet
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {
+        props.requests.map(request => {
+          return (
+            <h2>
+              {request.requestId}
+            </h2>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default RequestList
