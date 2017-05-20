10fc371c0ec6786c71937c26052a246eac9841b3
Jason Luo <jasonl960523@gmail.com>
2017-05-20 21:08:07 +0800

    Update actionTypes
    To have UPDATE_FIELD_AUTH, LOGIN and ASYNC_START

--- UniMemo App/react-app/src/constants/actionTypes.js
+++ UniMemo App/react-app/src/constants/actionTypes.js
@@ -1,7 +1,7 @@
 export const HOME_PAGE_LOADED = 'HOME_PAGE_LOADED'
-// export const
-// export const
-// export const
+export const UPDATE_FIELD_AUTH = 'UPDATE_FIELD_AUTH'
+export const LOGIN = 'LOGIN'
+export const ASYNC_START = 'ASYNC_START'
 // export const
 // export const
 // export const
