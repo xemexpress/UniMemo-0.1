8264c5d4d353fef5a5073be04268c9b58a0275c4
Jason Luo <jasonl960523@gmail.com>
2017-05-20 21:09:42 +0800

    Update auth's reducer
    Handle UPDATE_FIELD_AUTH, ASYNC_START and LOGIN

--- UniMemo App/react-app/src/reducers/auth.js
+++ UniMemo App/react-app/src/reducers/auth.js
@@ -1,5 +1,35 @@
-export default (state={}, action) => {
+import {
+  UPDATE_FIELD_AUTH,
+  ASYNC_START,
+  LOGIN
+} from '../constants/actionTypes'
+
+const defaultState = {
+  email: '',
+  password: ''
+}
+
+export default (state = defaultState, action) => {
   switch(action.type){
+    case UPDATE_FIELD_AUTH:
+      return {
+        ...state,
+        [action.key]: action.value
+      }
+    case ASYNC_START:
+      if(action.subtype === LOGIN){
+        return {
+          ...state,
+          inProgress: true
+        }
+      }
+      break
+    case LOGIN:
+      return {
+        ...state,
+        inProgress: false,
+        errors: action.error ? action.payload : null
+      }
     default:
   }
   return state
