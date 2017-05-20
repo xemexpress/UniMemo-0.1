d78aa9b0500492ec20363bcbc0a6690b30830a98
Jason Luo <jasonl960523@gmail.com>
2017-05-20 19:01:05 +0800

    Wire up Home and Login via Link

--- UniMemo App/react-app/src/components/Header.js
+++ UniMemo App/react-app/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 class Header extends React.Component {
   render(){
@@ -6,9 +7,24 @@ class Header extends React.Component {
       <nav className='navbar navbar-light'>
         <div className='container'>
 
-          <a className='navbar-brand'>
+          <Link to='/' className='navbar-brand'>
             {this.props.appName}
-          </a>
+          </Link>
+
+          <ul className='nav navbar-nav pull-xs-right'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link'>
+                Home
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link to='login' className='nav-link'>
+                Sign in
+              </Link>
+            </li>
+          </ul>
+
         </div>
       </nav>
     )
