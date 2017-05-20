0d99c5cedf6fad47fcfa152ddffb82705870e082
Jason Luo <jasonl960523@gmail.com>
2017-05-20 21:08:37 +0800

    Update promiseMiddleware to have ASYNC_START

--- UniMemo App/react-app/src/middleware.js
+++ UniMemo App/react-app/src/middleware.js
@@ -1,11 +1,20 @@
+import {
+  ASYNC_START
+} from './constants/actionTypes'
+
 const promiseMiddleware = store => next => action => {
   if(isPromise(action.payload)){
+    store.dispatch({
+      type: ASYNC_START,
+      subtype: action.type
+    })
     action.payload.then(
       res => {
         action.payload = res
         store.dispatch(action)
       },
       error => {
+        console.log(error)
         action.error = true
         action.payload = error.response.body
         store.dispatch(action)
