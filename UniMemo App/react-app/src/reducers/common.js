87e3018aee80596ac4e77bc8a29e45c2b3bcfa45
Jason Luo <jasonl960523@gmail.com>
2017-05-21 20:41:50 +0800

    Update common's reducer to handle APP_LOAD, REDIRECT and LOGIN

--- UniMemo App/react-app/src/reducers/common.js
+++ UniMemo App/react-app/src/reducers/common.js
@@ -1,9 +1,35 @@
+import {
+  APP_LOAD,
+  REDIRECT,
+  LOGIN
+} from '../constants/actionTypes'
+
 const defaultState = {
-  appName: 'UniMemo'
+  appName: 'UniMemo',
+  token: null
 }
 
 export default (state=defaultState, action) => {
   switch(action.type){
+    case APP_LOAD:
+      return {
+        ...state,
+        token: action.token || null,
+        appLoaded: true,
+        currentUser: action.payload ? action.payload.user : null
+      }
+    case REDIRECT:
+      return {
+        ...state,
+        redirectTo: null
+      }
+    case LOGIN:
+      return {
+        ...state,
+        redirectTo: action.error ? null : '/',
+        token: action.error ? null : action.payload.user.token,
+        currentUser: action.error ? null : action.payload.user
+      }
     default:
   }
   return state
