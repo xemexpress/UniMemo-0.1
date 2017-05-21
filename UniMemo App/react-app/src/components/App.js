8e8baf12fc158cd9d8f510b31cb1598afb21b43a
Jason Luo <jasonl960523@gmail.com>
2017-05-21 20:20:30 +0800

    Update App to get token and redirectTo prepared when loaded

--- UniMemo App/react-app/src/components/App.js
+++ UniMemo App/react-app/src/components/App.js
@@ -3,16 +3,52 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import Header from './Header'
+import agent from '../agent'
+
+import {
+  REDIRECT,
+  APP_LOAD
+} from '../constants/actionTypes'
 
 const mapStateToProps = state => ({
-  appName: state.common.appName
+  appName: state.common.appName,
+  currentUser: state.common.currentUser,
+  redirectTo: state.common.redirectTo
+})
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) => dispatch({
+    type: APP_LOAD,
+    payload,
+    token
+  }),
+  onRedirect: () => dispatch({
+    type: REDIRECT
+  })
 })
 
 class App extends React.Component {
+  componentWillMount(){
+    const token = window.localStorage.getItem('jwt')
+    if(token){
+      agent.setToken(token)
+    }
+    this.props.onLoad(token ? agent.Auth.current() : null, token)
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.redirectTo){
+      this.context.router.replace(nextProps.redirectTo)
+      this.props.onRedirect()
+    }
+  }
+
   render(){
     return (
       <div>
-        <Header appName={this.props.appName} />
+        <Header
+          appName={this.props.appName}
+          currentUser={this.props.currentUser} />
         {this.props.children}
       </div>
     )
@@ -23,4 +59,4 @@ App.contextTypes = {
   router: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps, () => ({}))(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
