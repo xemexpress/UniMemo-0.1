e15ee6ab6786c140aaa162eb7d96957effdf7122
Jason Luo <jasonl960523@gmail.com>
2017-05-21 20:32:53 +0800

    Update actionTypes

--- UniMemo App/react-app/src/constants/actionTypes.js
+++ UniMemo App/react-app/src/constants/actionTypes.js
@@ -2,8 +2,8 @@ export const HOME_PAGE_LOADED = 'HOME_PAGE_LOADED'
 export const UPDATE_FIELD_AUTH = 'UPDATE_FIELD_AUTH'
 export const LOGIN = 'LOGIN'
 export const ASYNC_START = 'ASYNC_START'
-// export const
-// export const
+export const APP_LOAD = 'APP_LOAD'
+export const REDIRECT = 'REDIRECT'
 // export const
 // export const
 // export const
