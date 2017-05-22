4cfb23ddce5a8d9d38ae094d8719958d1dba1da2
Jason Luo <jasonl960523@gmail.com>
2017-05-22 11:27:42 +0800

    Add settings's reducer to the store

--- UniMemo App/react-app/src/store.js
+++ UniMemo App/react-app/src/store.js
@@ -3,12 +3,14 @@ import { applyMiddleware, createStore, combineReducers } from 'redux'
 import common from './reducers/common'
 import auth from './reducers/auth'
 import home from './reducers/home'
+import settings from './reducers/settings'
 import { promiseMiddleware, localStorageMiddleware } from './middleware'
 
 const reducer = combineReducers({
   common,
   auth,
-  home
+  home,
+  settings
 })
 
 const middleware = applyMiddleware(promiseMiddleware, localStorageMiddleware)
