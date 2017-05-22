c75908e2263812a0e71d9d9ab2e515c860c8a172
Jason Luo <jasonl960523@gmail.com>
2017-05-22 11:30:54 +0800

    Update common's reducer
    Handle SETTINGS_SAVED and LOGOUT

--- UniMemo App/react-app/src/reducers/common.js
+++ UniMemo App/react-app/src/reducers/common.js
@@ -2,7 +2,9 @@ import {
   APP_LOAD,
   REDIRECT,
   LOGIN,
-  REGISTER
+  REGISTER,
+  SETTINGS_SAVED,
+  LOGOUT
 } from '../constants/actionTypes'
 
 const defaultState = {
@@ -31,6 +33,19 @@ export default (state=defaultState, action) => {
         redirectTo: action.error ? null : '/',
         token: action.error ? null : action.payload.user.token,
         currentUser: action.error ? null : action.payload.user
+      }
+    case SETTINGS_SAVED:
+      return {
+        ...state,
+        redirectTo: action.error ? null : '/',
+        currentUser: action.error ? null : action.payload.user
+      }
+    case LOGOUT:
+      return {
+        ...state,
+        redirectTo: '/',
+        currentUser: null,
+        token: null
       }
     default:
   }
