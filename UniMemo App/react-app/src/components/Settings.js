6ccb8c760a20be86eff0091e821d6d58c815f20a
Jason Luo <jasonl960523@gmail.com>
2017-05-22 22:23:12 +0800

    Update Settings to have SettingsForm and proPic displayed

--- UniMemo App/react-app/src/components/Settings.js
+++ UniMemo App/react-app/src/components/Settings.js
@@ -9,9 +9,123 @@ import {
   LOGOUT
 } from '../constants/actionTypes'
 
+class SettingsForm extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      proPic: '',
+      username: '',
+      bio: '',
+      password: '',
+      mobileNum: ''
+    }
+
+    this.updateState = field => ev => {
+      const newState = Object.assign(this.state, { [field]: ev.target.value })
+      this.setState(newState)
+    }
+
+    this.submitForm = ev => {
+      ev.preventDefault()
+
+      const user = this.state
+      if(!user.password){
+        delete user.password
+      }
+
+      this.props.onSubmitForm(user)
+    }
+  }
+
+  componentWillMount(){
+    if(this.props.currentUser){
+      this.setState({
+        proPic: this.props.currentUser.proPic || '',
+        username: this.props.currentUser.username,
+        bio: this.props.currentUser.bio,
+        mobileNum: this.props.currentUser.mobileNum
+      })
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.currentUser){
+      this.setState({
+        proPic: nextProps.currentUser.proPic || '',
+        username: nextProps.currentUser.username,
+        bio: nextProps.currentUser.bio,
+        mobileNum: nextProps.currentUser.mobileNum
+      })
+    }
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.submitForm}>
+        <fieldset>
+          <fieldset className='form-group'>
+            <input
+              className='form-control form-control-lg'
+              type='text'
+              placeholder='URL of profile picture'
+              value={this.state.proPic}
+              onChange={this.updateState('proPic')} />
+          </fieldset>
+
+          <fieldset className='form-group'>
+            <input
+              className='form-control form-control-lg'
+              type='text'
+              placeholder='username'
+              value={this.state.username}
+              onChange={this.updateState('username')} />
+          </fieldset>
+
+          <fieldset className='form-group'>
+            <input
+              className='form-control form-control-lg'
+              type='password'
+              placeholder='New Password'
+              value={this.state.username}
+              onChange={this.updateState('password')} />
+          </fieldset>
+
+          <fieldset className='form-group'>
+            <textarea
+              className='form-control form-control-lg'
+              rows='8'
+              placeholder='Short bio about you'
+              value={this.state.bio}
+              onChange={this.updateState('bio')}>
+            </textarea>
+          </fieldset>
+
+          <fieldset className='form-group'>
+            <input
+              className='form-control form-control-lg'
+              type='tel'
+              placeholder='Mobile Number'
+              value={this.state.mobileNum}
+              onChange={this.updateState('mobileNum')} />
+          </fieldset>
+
+          <button
+            className='btn btn-lg btn-primary pull-xs-right'
+            type='submit'
+            disabled={this.props.inProgress}>
+            Update Settings
+          </button>
+
+        </fieldset>
+      </form>
+    )
+  }
+}
+
 const mapStateToProps = state => ({
   ...state.settings,
-  state.common.currentUser
+  currentUser: state.common.currentUser
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -35,6 +149,34 @@ class Settings extends React.Component {
               <h1 className='text-xs-center'>
                 Your Settings
               </h1>
+
+              {
+                this.props.currentUser ?
+                  <div className='text-xs-center'>
+                    <i className='ion-star'></i>&nbsp;
+                    {this.props.currentUser.yellowStars}&nbsp;&nbsp;
+                    Mem&nbsp;
+                    {this.props.currentUser.mem}
+                    <br />
+                    <img
+                      className='img-fluid'
+                      src={ this.props.currentUser.proPic ? this.props.currentUser.proPic : 'https://photouploads.com/images/350646.png' }
+                      alt={`${this.props.currentUser.username}'s proPic`} />
+                  </div> : null
+              }
+
+              <br />
+
+              <div className='text-xs-center'>
+                <i className='ion-information-circled'></i>&nbsp;
+                Get URL by uploading your custom pic&nbsp;
+                <a
+                  className='nav-link'
+                  href='https://photouploads.com/'
+                  target='_blank'>here</a>
+              </div>
+
+              <hr />
 
               <ListErrors errors={this.props.errors} />
 
