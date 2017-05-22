7017ededdcc8d7a4dc34e38c4a15438669625b2e
Jason Luo <jasonl960523@gmail.com>
2017-05-22 09:42:07 +0800

    Init Settings

---
+++ UniMemo App/react-app/src/components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class Settings extends React.Component {
+  render(){
+    return (
+      null
+    )
+  }
+}
+
+export default connect(()=>({}), ()=>({}))(Settings)
