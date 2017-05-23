eeeac4e349d473e518bdae0f2b0065e826db7e63
Jason Luo <jasonl960523@gmail.com>
2017-05-23 15:51:35 +0800

    Integrate request's reducer into the store

--- UniMemo App/react-app/src/store.js
+++ UniMemo App/react-app/src/store.js
@@ -4,13 +4,15 @@ import common from './reducers/common'
 import auth from './reducers/auth'
 import home from './reducers/home'
 import settings from './reducers/settings'
+import request from './reducers/request'
 import { promiseMiddleware, localStorageMiddleware } from './middleware'
 
 const reducer = combineReducers({
   common,
   auth,
   home,
-  settings
+  settings,
+  request
 })
 
 const middleware = applyMiddleware(promiseMiddleware, localStorageMiddleware)
