a508ce06fdd3e1b7ff53db28cca1c1d2ba26d2cc
Jason Luo <jasonl960523@gmail.com>
2017-05-24 12:58:08 +0800

    Update RequestMeta

--- UniMemo App/react-app/src/components/Request/RequestMeta.js
+++ UniMemo App/react-app/src/components/Request/RequestMeta.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-class RequestMeta extends React.Component {
-  render(){
-    return (
-      null
-    )
-  }
+import RequestActions from './RequestActions'
+
+const RequestMeta = ({ request, canModify }) => {
+  return (
+    <div className='article-meta'>
+
+      <Link to=''>
+        <img src={request.poster.proPic} alt={request.poster.username} />
+      </Link>
+
+      <div className='info'>
+        <Link to='' className='author'>
+          {request.poster.username}
+        </Link>
+        <span className='date'>
+          {new Date(request.createdAt).toDateString()}
+        </span>
+      </div>
+
+      <RequestActions canModify={canModify} request={request} />
+    
+    </div>
+  )
 }
 
 export default RequestMeta
