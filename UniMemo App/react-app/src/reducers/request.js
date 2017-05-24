3ad9938d4e64e9ad2cbaf8619553f8c71990e834
Jason Luo <jasonl960523@gmail.com>
2017-05-24 15:32:45 +0800

    Update request's reducer
    Update comments when Comment is deleted

--- UniMemo App/react-app/src/reducers/request.js
+++ UniMemo App/react-app/src/reducers/request.js
@@ -1,7 +1,8 @@
 import {
   REQUEST_PAGE_LOADED,
   REQUEST_PAGE_UNLOADED,
-  ADD_COMMENT
+  ADD_COMMENT,
+  DELETE_COMMENT
 } from '../constants/actionTypes'
 
 export default (state={}, action) => {
@@ -20,6 +21,11 @@ export default (state={}, action) => {
         commentErrors: action.error ? action.payload.errors : null,
         comments: action.error ? null :
           [action.payload.comment].concat(state.comments || [])
+      }
+    case DELETE_COMMENT:
+      return {
+        ...state,
+        comments: state.comments.filter(comment => action.commentId !== comment.id)
       }
     default:
   }
