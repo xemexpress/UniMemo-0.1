e89763f641e04ee26993c02332d5640bb3cfa4d2
Jason Luo <jasonl960523@gmail.com>
2017-05-24 21:57:38 +0800

    Update home's reducer

--- UniMemo App/react-app/src/reducers/home.js
+++ UniMemo App/react-app/src/reducers/home.js
@@ -1,14 +1,11 @@
 import {
-  HOME_PAGE_LOADED
+  HOME_PAGE_UNLOADED
 } from '../constants/actionTypes'
 
 export default (state = {}, action) => {
   switch(action.type){
-    case HOME_PAGE_LOADED:
-      return {
-        ...state,
-        requests: action.payload.requests
-      }
+    case HOME_PAGE_UNLOADED:
+      return {}
     default:
   }
   return state
