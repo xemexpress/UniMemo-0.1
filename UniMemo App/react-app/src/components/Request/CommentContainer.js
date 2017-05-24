4b7d89bf9c857886a82be6271d18aa5408b074eb
Jason Luo <jasonl960523@gmail.com>
2017-05-24 12:50:53 +0800

    Update CommentContainer
    Handle cases of logged-in or not

--- UniMemo App/react-app/src/components/Request/CommentContainer.js
+++ UniMemo App/react-app/src/components/Request/CommentContainer.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-class CommentContainer extends React.Component {
-  render(){
+import CommentList from './CommentList'
+import CommentInput from './CommentInput'
+
+const CommentContainer = props => {
+  if(props.currentUser){
     return (
-      null
+      <div className='col-xs-12 col-md-8 offset-md-2'>
+        <CommentList
+          comments={props.comments}
+          requestId={props.requestId}
+          currentUser={props.currentUser} />
+
+        <CommentInput
+          errors={props.errors}
+          requestId={props.requestId}
+          currentUser={props.currentUser} />
+      </div>
     )
   }
+
+  return (
+    <div className='col-xs-12 col-md-8 offset-md-2'>
+      <CommentList
+        comments={props.comments}
+        requestId={props.requestId}
+        currentUser={props.currentUser} />
+
+      <p>
+        <Link to='login'>Sign in</Link>
+      &nbsp;or&nbsp;
+        <Link to='register'>Sign up</Link>
+      &nbsp;to add comments to this request
+      </p>
+    </div>
+  )
 }
 
 export default CommentContainer
