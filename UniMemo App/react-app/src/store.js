d40c2cd53969b48f9bce169de1197fad2720721c
Jason Luo <jasonl960523@gmail.com>
2017-05-24 22:01:21 +0800

    Add profile's, request's, requestList's reducers to the store

--- UniMemo App/react-app/src/store.js
+++ UniMemo App/react-app/src/store.js
@@ -3,16 +3,20 @@ import { applyMiddleware, createStore, combineReducers } from 'redux'
 import common from './reducers/common'
 import auth from './reducers/auth'
 import home from './reducers/home'
+import profile from './reducers/profile'
 import settings from './reducers/settings'
 import request from './reducers/request'
+import requestList from './reducers/requestList'
 import { promiseMiddleware, localStorageMiddleware } from './middleware'
 
 const reducer = combineReducers({
   common,
   auth,
   home,
+  profile,
   settings,
-  request
+  request,
+  requestList
 })
 
 const middleware = applyMiddleware(promiseMiddleware, localStorageMiddleware)
