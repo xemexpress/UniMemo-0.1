677df5ffd847f3d6e3fc20f3cbf8929e87053496
Jason Luo <jasonl960523@gmail.com>
2017-05-24 13:00:40 +0800

    Create RequestAction

---
+++ UniMemo App/react-app/src/components/Request/RequestActions.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+
+import agent from '../../agent'
+
+import {
+  DELETE_REQUEST
+} from '../../constants/actionTypes'
+
+const mapDispatchToProps = dispatch => ({
+  onClickDelete: payload => dispatch({
+    type: DELETE_REQUEST,
+    payload
+  })
+})
+
+const RequestActions = props => {
+  const request = props.request
+  const del = () => {
+    props.onClickDelete(agent.Requests.del(request.requestId))
+  }
+
+  if(props.canModify){
+    return (
+      <span>
+        <Link
+          to=''
+          className='btn btn-outline-secondary btn-sm'>
+          <i className='ion-edit'></i> Edit Request
+        </Link>
+
+        <button
+          className='btn btn-outline-danger btn-sm'
+          onClick={del}>
+          <i className='ion-trash-a'></i> Delete Request
+        </button>
+      </span>
+    )
+  }
+
+  return (
+    null
+  )
+}
+
+export default connect(()=>({}), mapDispatchToProps)(RequestActions)
