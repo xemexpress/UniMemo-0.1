5ae143b09ef846b63eebbe7596de16eae4684c66
Jason Luo <jasonl960523@gmail.com>
2017-05-25 23:17:31 +0800

    Update home's reducer to load tags when HOME_PAGE_LOADED

--- UniMemo App/react-app/src/reducers/home.js
+++ UniMemo App/react-app/src/reducers/home.js
@@ -1,9 +1,14 @@
 import {
+  HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED
 } from '../constants/actionTypes'
 
 export default (state = {}, action) => {
   switch(action.type){
+    case HOME_PAGE_LOADED:
+      return {
+        tags: action.payload[0].request_tags
+      }
     case HOME_PAGE_UNLOADED:
       return {}
     default:
