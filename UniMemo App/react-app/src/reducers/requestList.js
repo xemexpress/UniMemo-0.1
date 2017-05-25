14122c7435f93946231ad0dc7a8e754b29661e83
Jason Luo <jasonl960523@gmail.com>
2017-05-25 23:19:28 +0800

    Update requestList's reducer for actions related to tabs, tags and pages

--- UniMemo App/react-app/src/reducers/requestList.js
+++ UniMemo App/react-app/src/reducers/requestList.js
@@ -2,7 +2,10 @@ import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
   PROFILE_PAGE_LOADED,
-  PROFILE_PAGE_UNLOADED
+  PROFILE_PAGE_UNLOADED,
+  CHANGE_TAB,
+  APPLY_TAG_FILTER,
+  SET_PAGE
 } from '../constants/actionTypes'
 
 export default (state={}, action) => {
@@ -10,7 +13,11 @@ export default (state={}, action) => {
     case HOME_PAGE_LOADED:
       return {
         ...state,
-        requests: action.payload.requests
+        tab: action.tab,
+        tag: null,
+        requests: action.payload[1].requests,
+        requestsCount: action.payload[1].requestsCount,
+        currentPage: 0
       }
     case HOME_PAGE_UNLOADED:
       return {}
@@ -18,10 +25,36 @@ export default (state={}, action) => {
       return {
         ...state,
         requests: action.payload[1].requests,
-        requestsCount: action.payload[1].requestsCount
+        requestsCount: action.payload[1].requestsCount,
+        currentPage: 0
       }
     case PROFILE_PAGE_UNLOADED:
       return {}
+    case CHANGE_TAB:
+      return {
+        ...state,
+        tag: null,
+        tab: action.tab,
+        requests: action.payload.requests,
+        requestsCount: action.payload.requestsCount,
+        currentPage: 0
+      }
+    case APPLY_TAG_FILTER:
+      return {
+        ...state,
+        tag: action.tag,
+        tab: null,
+        requests: action.payload.requests,
+        requestsCount: action.payload.requestsCount,
+        currentPage: 0
+      }
+    case SET_PAGE:
+      return {
+        ...state,
+        currentPage: action.page,
+        requests: action.payload.requests,
+        requestsCount: action.payload.requestsCount
+      }
     default:
   }
   return state
