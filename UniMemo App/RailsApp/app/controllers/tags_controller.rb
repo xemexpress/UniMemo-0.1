dac3da20b1b3d9973c9856fa37d07c3a3465bfbd
Jason Luo <jasonl960523@gmail.com>
2017-05-25 23:08:01 +0800

    Filter off  Request's status tags

--- UniMemo App/RailsApp/app/controllers/tags_controller.rb
+++ UniMemo App/RailsApp/app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def index
     render json: {
-      request_tags: Request.tag_counts.most_used.map(&:name),
+      request_tags: Request.tag_counts.most_used.map(&:name) - ['done', 'ongoing'],
       gift_tags: Gift.tag_counts.most_used.map(&:name)
      }
   end
