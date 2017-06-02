ff560f8b6120c513975ee3dac6a58345d454bd83
Jason Luo <jasonl960523@gmail.com>
2017-06-02 16:56:44 +0800

    Update Tags's controller
    Separate Tags by Request and Gift type

--- UniMemo App/RailsApp/app/controllers/tags_controller.rb
+++ UniMemo App/RailsApp/app/controllers/tags_controller.rb
@@ -5,13 +5,15 @@ class TagsController < ApplicationController
         request_tags: Request.all.tagged_with('ongoing').tag_counts.most_used.map(&:name) - ['done', 'ongoing', 'ongoing-taken']
       }
     elsif params[:tag] == 'gifts'
+      @open_public_gifts = Gift.tagged_with("openPublic").map(&:gift_id)
       render json: {
-        gift_tags: Gift.tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
+        gift_tags: Gift.all.related_to(@current_user_id).or(Gift.tagged(@open_public_gifts)).tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
       }
     else
+      @open_public_gifts = Gift.tagged_with("openPublic").map(&:gift_id)
       render json: {
         request_tags: Request.all.tagged_with('ongoing').tag_counts.most_used.map(&:name) - ['done', 'ongoing', 'ongoing-taken'],
-        gift_tags: Gift.tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
+        gift_tags: Gift.all.related_to(@current_user_id).or(Gift.tagged(@open_public_gifts)).tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
       }
     end
   end
