a3b62a5ab492f79c1dd7eaee29af6bca94c9c3fc
Jason Luo <jasonl960523@gmail.com>
2017-06-04 02:21:41 +0800

    BSR Tag's controller

--- UniMemo App/RailsApp/app/controllers/tags_controller.rb
+++ UniMemo App/RailsApp/app/controllers/tags_controller.rb
@@ -1,19 +1,22 @@
 class TagsController < ApplicationController
   def index
+    @requests = Request.all.tagged_with('ongoing')
+
+    @open_public_gifts = Gift.tagged_with("openPublic").map(&:gift_id)
+    @gifts = Gift.where(receiver: current_user).or(Gift.tagged(@open_public_gifts))
+
     if params[:tag] == 'requests'
       render json: {
-        request_tags: Request.all.tagged_with('ongoing').tag_counts.most_used.map(&:name) - ['done', 'ongoing', 'ongoing-taken']
+        request_tags: @requests.tag_counts.most_used.map(&:name) - ['done', 'ongoing', 'ongoing-taken']
       }
     elsif params[:tag] == 'gifts'
-      @open_public_gifts = Gift.tagged_with("openPublic").map(&:gift_id)
       render json: {
-        gift_tags: Gift.all.related_to(@current_user_id).or(Gift.tagged(@open_public_gifts)).tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
+        gift_tags: @gifts.tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
       }
     else
-      @open_public_gifts = Gift.tagged_with("openPublic").map(&:gift_id)
       render json: {
-        request_tags: Request.all.tagged_with('ongoing').tag_counts.most_used.map(&:name) - ['done', 'ongoing', 'ongoing-taken'],
-        gift_tags: Gift.all.related_to(@current_user_id).or(Gift.tagged(@open_public_gifts)).tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
+        request_tags: @requests.tag_counts.most_used.map(&:name) - ['done', 'ongoing', 'ongoing-taken'],
+        gift_tags: @gifts.tag_counts.most_used.map(&:name) - ['personal', 'public', 'openPublic']
       }
     end
   end
